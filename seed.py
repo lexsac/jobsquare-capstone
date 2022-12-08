78ecb97b9e95ec113b9d2461c02ae6c3a46c8ccd
Lexsa Campbell <lexsacampbell@gmail.com>
2022-12-07 18:35:35 -0800

    Added comments for refactoring questions

--- seed.py
+++ seed.py
@@ -1,12 +1,12 @@
 """Seed file to make sample data for db."""
 
-from models import User, Job, UserJob, db, Location, Category, Experiencelevel, Company
+from models import db, Job, Location, Category, Experiencelevel, Company
 from app import app
 import requests
 from key import API_SECRET_KEY
 
 API_BASE_URL = "https://www.themuse.com/api/public/jobs"
-job_list= []
+
 
 # Create all tables
 with app.app_context():     
@@ -46,3 +46,41 @@ def get_jobs(start_page_num, end_page_num):
                 db.session.commit()
 
 get_jobs(1,20)
+
+
+# def get_jobs(start_page_num, end_page_num):
+
+#     for page_num in range(start_page_num, end_page_num):
+#         resp = requests.get(f"{API_BASE_URL}?page={page_num}", params={"key": API_SECRET_KEY}).json()
+
+#     return resp['results']
+
+
+# response = get_jobs(1,20)
+
+# for job_info in response:
+#     with app.app_context():
+#         j = Job(name=job_info['name'],
+#             description=job_info['contents'],
+#             location=job_info['locations'][0]['name'],
+#             # category=job_info['categories'][0]['name'],
+#             category = "filler text",
+#             experience_level=job_info['levels'][0]['name'],
+#             company=job_info['company']['name'],
+#             landing_page_url=job_info['refs']['landing_page'] 
+#             )
+#         if (Location.query.filter_by(name=job_info['locations'][0]['name']).first() == None):
+#             l = Location(name=job_info['locations'][0]['name'])
+#             db.session.add(l)
+#         if (Category.query.filter_by(name=job_info['locations'][0]['name']).first() == None):
+#             ca = Category(name=job_info['locations'][0]['name'])
+#             db.session.add(ca)
+#         if (Experiencelevel.query.filter_by(name=job_info['levels'][0]['name']).first() == None):
+#             e = Experiencelevel(name=job_info['levels'][0]['name'])
+#             db.session.add(e)
+#         if (Company.query.filter_by(name=job_info['company']['name']).first() == None):
+#             co = Company(name=job_info['company']['name'])
+#             db.session.add(co)
+        
+#         db.session.add(j)
+#         db.session.commit()
