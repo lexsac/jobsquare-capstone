cae77cfa6e0601f8d9cfce2d36f7776e45ce9af1
Lexsa Campbell <lexsacampbell@gmail.com>
2023-02-27 14:14:38 -0800

    Added logic to account for missing db job attributes, changed foreign key attributes in job table to ids, not names

--- seed.py
+++ seed.py
@@ -1,6 +1,6 @@
 """Seed file to make sample data for db."""
 
-from models import db, Job, Location, Category, Experiencelevel, Company
+from models import db, Job, Location, Category, Experiencelevel, Company, User, UserJob
 from app import app
 import requests
 from key import API_SECRET_KEY
@@ -22,68 +22,36 @@ with app.app_context():
             
             for job_info in response['results']:
                 with app.app_context():
+                    if (job_info['locations'] and Location.query.filter_by(name=job_info['locations'][0]['name']).first() == None):
+                        l = Location(name=job_info['locations'][0]['name'])
+                        db.session.add(l)
+                        location_id = Location.query.filter_by(name=job_info['locations'][0]['name']).first().id
+
+                    if (job_info['categories'] and Category.query.filter_by(name=job_info['categories'][0]['name']).first() == None):
+                        ca = Category(name=job_info['categories'][0]['name'])
+                        db.session.add(ca)
+                        category_id = Category.query.filter_by(name=job_info['categories'][0]['name']).first().id
+
+                    if (job_info['levels'] and Experiencelevel.query.filter_by(name=job_info['levels'][0]['name']).first() == None):
+                        e = Experiencelevel(name=job_info['levels'][0]['name'])
+                        db.session.add(e)
+                        experience_id = Experiencelevel.query.filter_by(name=job_info['levels'][0]['name']).first().id
+
+                    if (job_info['company'] and Company.query.filter_by(name=job_info['company']['name']).first() == None):
+                        co = Company(name=job_info['company']['name'])
+                        db.session.add(co)
+                        company_id = Company.query.filter_by(name=job_info['company']['name']).first().id
+
                     j = Job(name=job_info['name'],
-                        description=job_info['contents'],
-                        location=job_info['locations'][0]['name'],
-                        # category=job_info['categories'][0]['name'],
-                        category = "filler text",
-                        experience_level=job_info['levels'][0]['name'],
-                        company=job_info['company']['name'],
+                        description=job_info['contents'] if job_info['contents'] else None,
+                        location=location_id if job_info['locations'] else None,
+                        category=category_id if job_info['categories'] else None,
+                        experience_level=experience_id,
+                        company=company_id if job_info['company'] else None,
                         created_at=job_info['publication_date'],
                         landing_page_url=job_info['refs']['landing_page'] 
                     )
-                    if (Location.query.filter_by(name=job_info['locations'][0]['name']).first() == None):
-                        l = Location(name=job_info['locations'][0]['name'])
-                        db.session.add(l)
-                    if (Category.query.filter_by(name=job_info['locations'][0]['name']).first() == None):
-                        ca = Category(name=job_info['locations'][0]['name'])
-                        db.session.add(ca)
-                    if (Experiencelevel.query.filter_by(name=job_info['levels'][0]['name']).first() == None):
-                        e = Experiencelevel(name=job_info['levels'][0]['name'])
-                        db.session.add(e)
-                    if (Company.query.filter_by(name=job_info['company']['name']).first() == None):
-                        co = Company(name=job_info['company']['name'])
-                        db.session.add(co)
                     db.session.add(j)
                     db.session.commit()
 
-    get_jobs(0,20)
-
-
-
-# def get_jobs(start_page_num, end_page_num):
-
-#     for page_num in range(start_page_num, end_page_num):
-#         resp = requests.get(f"{API_BASE_URL}?page={page_num}", params={"key": API_SECRET_KEY}).json()
-
-#     return resp['results']
-
-
-# response = get_jobs(1,20)
-
-# for job_info in response:
-#     with app.app_context():
-#         j = Job(name=job_info['name'],
-#             description=job_info['contents'],
-#             location=job_info['locations'][0]['name'],
-#             # category=job_info['categories'][0]['name'],
-#             category = "filler text",
-#             experience_level=job_info['levels'][0]['name'],
-#             company=job_info['company']['name'],
-#             landing_page_url=job_info['refs']['landing_page'] 
-#             )
-#         if (Location.query.filter_by(name=job_info['locations'][0]['name']).first() == None):
-#             l = Location(name=job_info['locations'][0]['name'])
-#             db.session.add(l)
-#         if (Category.query.filter_by(name=job_info['locations'][0]['name']).first() == None):
-#             ca = Category(name=job_info['locations'][0]['name'])
-#             db.session.add(ca)
-#         if (Experiencelevel.query.filter_by(name=job_info['levels'][0]['name']).first() == None):
-#             e = Experiencelevel(name=job_info['levels'][0]['name'])
-#             db.session.add(e)
-#         if (Company.query.filter_by(name=job_info['company']['name']).first() == None):
-#             co = Company(name=job_info['company']['name'])
-#             db.session.add(co)
-        
-#         db.session.add(j)
-#         db.session.commit()
+    get_jobs(0,99)
