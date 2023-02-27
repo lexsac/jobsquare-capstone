153a082d24e636ac5b1740906669b5ae22d944d4
Lexsa Campbell <lexsacampbell@gmail.com>
2023-02-27 14:12:48 -0800

    Changed URL for heroku postgres

--- app.py
+++ app.py
@@ -13,9 +13,10 @@ CURR_USER_KEY = "curr_user"
 app = Flask(__name__)
 
 
-uri = os.getenv('DATABASE_URL')  # or other relevant config var
-if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
+uri = 'DATABASE_URL'  # or other relevant config var
+
+if uri.startswith('postgres://'):
+    uri = uri.replace('postgres://', 'postgresql://', 1)
 
 # Get DB_URI from environ variable (useful for production/testing) or,
 # if not set there, use development local db.
